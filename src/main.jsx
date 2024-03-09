a35ee693a30db712a26a465e6e626422ac5a4f64
Pruthviraj Chaudhari <chaudharipruthviraj888@gmail.com>
2024-03-09 23:06:38 +0530

    Change HashRouter to BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { Toaster } from "sonner";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import "@radix-ui/themes/styles.css";
 import "./index.css";
 import { Provider } from "react-redux";
@@ -9,9 +9,9 @@ import store from "./redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <HashRouter>
+    <BrowserRouter>
       <App />
       <Toaster position="top-right" richColors />
-    </HashRouter>
+    </BrowserRouter>
   </Provider>
 );
